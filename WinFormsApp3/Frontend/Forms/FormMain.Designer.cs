5f110564c39d5c639da740191857df9f7d0f8218
Yotam H <yotam4h@gmail.com>
2023-05-07 23:04:59 +0300

    page names

--- WinFormsApp3/Frontend/Forms/FormMain.Designer.cs
+++ WinFormsApp3/Frontend/Forms/FormMain.Designer.cs
@@ -31,26 +31,26 @@
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             materialTabControl1 = new ReaLTaiizor.Controls.MaterialTabControl();
-            tabPage1 = new TabPage();
-            dataGridView1 = new DataGridView();
-            tabPage2 = new TabPage();
-            tabPage3 = new TabPage();
-            tabPage4 = new TabPage();
-            menuIcons = new ImageList(components);
+            Home = new TabPage();
             tableLayoutPanel1 = new TableLayoutPanel();
+            dataGridView1 = new DataGridView();
+            Food = new TabPage();
+            Coffee = new TabPage();
+            SoftDrink = new TabPage();
+            menuIcons = new ImageList(components);
             materialTabControl1.SuspendLayout();
-            tabPage1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            Home.SuspendLayout();
             tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // materialTabControl1
             // 
             materialTabControl1.AccessibleName = "";
-            materialTabControl1.Controls.Add(tabPage1);
-            materialTabControl1.Controls.Add(tabPage2);
-            materialTabControl1.Controls.Add(tabPage3);
-            materialTabControl1.Controls.Add(tabPage4);
+            materialTabControl1.Controls.Add(Home);
+            materialTabControl1.Controls.Add(Food);
+            materialTabControl1.Controls.Add(Coffee);
+            materialTabControl1.Controls.Add(SoftDrink);
             materialTabControl1.Depth = 0;
             materialTabControl1.Dock = DockStyle.Fill;
             materialTabControl1.ImageList = menuIcons;
@@ -64,73 +64,17 @@
             materialTabControl1.Size = new Size(878, 440);
             materialTabControl1.TabIndex = 18;
             // 
-            // tabPage1
+            // Home
             // 
-            tabPage1.Controls.Add(tableLayoutPanel1);
-            tabPage1.ImageKey = "home_page_24px.png";
-            tabPage1.Location = new Point(4, 31);
-            tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(870, 405);
-            tabPage1.TabIndex = 0;
-            tabPage1.Text = "Home";
-            tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.AllowUserToAddRows = false;
-            dataGridView1.AllowUserToDeleteRows = false;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Dock = DockStyle.Fill;
-            dataGridView1.Location = new Point(435, 3);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.ReadOnly = true;
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(426, 393);
-            dataGridView1.TabIndex = 0;
-            // 
-            // tabPage2
-            // 
-            tabPage2.ImageKey = "hamburger_24px.png";
-            tabPage2.Location = new Point(4, 31);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(870, 405);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "Add Food";
-            tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tabPage3
-            // 
-            tabPage3.ImageKey = "cafe_24px.png";
-            tabPage3.Location = new Point(4, 31);
-            tabPage3.Name = "tabPage3";
-            tabPage3.Padding = new Padding(3);
-            tabPage3.Size = new Size(870, 405);
-            tabPage3.TabIndex = 2;
-            tabPage3.Text = "Add Coffee";
-            tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // tabPage4
-            // 
-            tabPage4.ImageKey = "soda_bottle_24px.png";
-            tabPage4.Location = new Point(4, 31);
-            tabPage4.Name = "tabPage4";
-            tabPage4.Padding = new Padding(3);
-            tabPage4.Size = new Size(870, 405);
-            tabPage4.TabIndex = 3;
-            tabPage4.Text = "Add Soft Drink";
-            tabPage4.UseVisualStyleBackColor = true;
-            // 
-            // menuIcons
-            // 
-            menuIcons.ColorDepth = ColorDepth.Depth8Bit;
-            menuIcons.ImageStream = (ImageListStreamer)resources.GetObject("menuIcons.ImageStream");
-            menuIcons.TransparentColor = Color.Transparent;
-            menuIcons.Images.SetKeyName(0, "cafe_24px.png");
-            menuIcons.Images.SetKeyName(1, "hamburger_24px.png");
-            menuIcons.Images.SetKeyName(2, "home_page_24px.png");
-            menuIcons.Images.SetKeyName(3, "soda_bottle_24px.png");
+            Home.Controls.Add(tableLayoutPanel1);
+            Home.ImageKey = "home_page_24px.png";
+            Home.Location = new Point(4, 31);
+            Home.Name = "Home";
+            Home.Padding = new Padding(3);
+            Home.Size = new Size(870, 405);
+            Home.TabIndex = 0;
+            Home.Text = "Home";
+            Home.UseVisualStyleBackColor = true;
             // 
             // tableLayoutPanel1
             // 
@@ -147,6 +91,62 @@
             tableLayoutPanel1.Size = new Size(864, 399);
             tableLayoutPanel1.TabIndex = 2;
             // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(435, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(426, 393);
+            dataGridView1.TabIndex = 0;
+            // 
+            // Food
+            // 
+            Food.ImageKey = "hamburger_24px.png";
+            Food.Location = new Point(4, 31);
+            Food.Name = "Food";
+            Food.Padding = new Padding(3);
+            Food.Size = new Size(870, 405);
+            Food.TabIndex = 1;
+            Food.Text = "Add Food";
+            Food.UseVisualStyleBackColor = true;
+            // 
+            // Coffee
+            // 
+            Coffee.ImageKey = "cafe_24px.png";
+            Coffee.Location = new Point(4, 31);
+            Coffee.Name = "Coffee";
+            Coffee.Padding = new Padding(3);
+            Coffee.Size = new Size(870, 405);
+            Coffee.TabIndex = 2;
+            Coffee.Text = "Add Coffee";
+            Coffee.UseVisualStyleBackColor = true;
+            // 
+            // SoftDrink
+            // 
+            SoftDrink.ImageKey = "soda_bottle_24px.png";
+            SoftDrink.Location = new Point(4, 31);
+            SoftDrink.Name = "SoftDrink";
+            SoftDrink.Padding = new Padding(3);
+            SoftDrink.Size = new Size(870, 405);
+            SoftDrink.TabIndex = 3;
+            SoftDrink.Text = "Add Soft Drink";
+            SoftDrink.UseVisualStyleBackColor = true;
+            // 
+            // menuIcons
+            // 
+            menuIcons.ColorDepth = ColorDepth.Depth8Bit;
+            menuIcons.ImageStream = (ImageListStreamer)resources.GetObject("menuIcons.ImageStream");
+            menuIcons.TransparentColor = Color.Transparent;
+            menuIcons.Images.SetKeyName(0, "cafe_24px.png");
+            menuIcons.Images.SetKeyName(1, "hamburger_24px.png");
+            menuIcons.Images.SetKeyName(2, "home_page_24px.png");
+            menuIcons.Images.SetKeyName(3, "soda_bottle_24px.png");
+            // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -160,20 +160,20 @@
             Padding = new Padding(0, 64, 3, 3);
             Text = "Menu Manager";
             materialTabControl1.ResumeLayout(false);
-            tabPage1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            Home.ResumeLayout(false);
             tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private ReaLTaiizor.Controls.MaterialTabControl materialTabControl1;
-        private TabPage tabPage1;
-        private TabPage tabPage2;
+        private TabPage Home;
+        private TabPage Food;
         private ImageList menuIcons;
-        private TabPage tabPage3;
-        private TabPage tabPage4;
+        private TabPage Coffee;
+        private TabPage SoftDrink;
         private DataGridView dataGridView1;
         private TableLayoutPanel tableLayoutPanel1;
     }
